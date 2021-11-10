8933c9ea2c2ef9cfe58c661be195c3e2e713c932
SerhiiHrynchyshyn <server_059@ukr.net>
2021-11-10 23:52:44 +0200

    leson 1 ( home + class )

---
+++ js-leson-1/main.js
@@ -0,0 +1,207 @@
+// Д\з
+/*let hello = 'hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+let num1 = 1;
+let num10 = 10;
+let num999 = -999;
+let num123 = 123;
+let num3 = 3.14;
+let num2 = 2.7;
+let num16 = 16;
+let boolTrue = true;
+let boolFalse = false;*/
+
+/*console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(num1);
+console.log(num10);
+console.log(num999);
+console.log(num123);
+console.log(num3);
+console.log(num2);
+console.log(num16);
+console.log(boolTrue);
+console.log(boolFalse);*/
+
+/*alert(hello);
+alert(owu);
+alert(com);
+alert(ua);
+alert(num1);
+alert(num10);
+alert(num999);
+alert(num123);
+alert(num3);
+alert(num2);
+alert(num16);
+alert(boolTrue);
+alert(boolFalse);*/
+
+/*document.write(hello);
+document.write(owu);
+document.write(com);
+document.write(ua);
+document.write(num1);
+document.write(num10);
+document.write(num999);
+document.write(num123);
+document.write(num3);
+document.write(num2);
+document.write(num16);
+document.write(boolTrue);
+document.write(boolFalse);*/
+
+/*hello = 'hello world';
+owu = 'okten';
+com = 'ua';
+ua = 'com';
+num1 = 2;
+num10 = 100;
+num999 = 999;
+num123 = 321;
+num3 = 14.3;
+num2 = 7.2;
+num16 = 26;
+boolTrue = false;
+boolFalse = true;
+
+console.log(hello, owu, com, ua, num1, num10, num999, num123, num3, num2, num16, boolTrue, boolFalse, console,);
+console.log(`${hello} ${owu} ${com} ${ua} ${num1} ${num10} ${num999} ${num123} ${num3} ${num2} ${num16} ${boolTrue} ${boolFalse}`);
+document.write(`${hello} ${owu} ${com} ${ua} ${num1} ${num10} ${num999} ${num123} ${num3} ${num2} ${num16} ${boolTrue} ${boolFalse}`);
+alert(`${hello} ${owu} ${com} ${ua} ${num1} ${num10} ${num999} ${num123} ${num3} ${num2} ${num16} ${boolTrue} ${boolFalse} `);*/
+
+
+/*let firstName = 'Serhii';
+let middleName = 'Yuriyovych';
+let lastName = 'Hrynchyshyn';
+
+let person = `${lastName} ${firstName} ${middleName}`;*/
+
+/*let firstName = prompt('вітаю Імя!');
+let middleName = prompt('По батькові');
+let age = prompt('вік!');
+alert('hello ' + firstName + ' ' + middleName + ' ' + age);*/
+
+/*
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+*/
+
+/*console.log(5 < 6)//5 ? 6 -> true
+console.log(5 > 6)//5 ? 6 -> false
+console.log(5 === 6)// 5 ? 6 -> false
+console.log(5 >= 6)// 5 ? 6 -> false
+console.log(10 === 10)// 10 ? 10 -> true
+console.log(10 >= 10)// 10 ? 10 -> true
+console.log(10 < 10)// 10 ? 10 -> false
+console.log(10 !== 10)// 10 ? 10 -> false
+console.log(10 != 10)// 10 ? 10 -> false
+console.log(123 === '123')// 123 ? '123' -> false
+console.log(123 == '123')// 123 ? '123' -> true*/
+
+/*let str = "20";
+let a = 5;
+document.write(str + a + "<br/>");
+document.write(str - a + "<br/>");
+document.write(str * "2" + "<br/>");
+document.write(str / 2 + "<br/>");*/
+// стрічка + число = конкантенація -->
+// при інших операторах стрічка повидить себе як число якщо там немає букв
+
+
+// homework
+// 1
+/*let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let result = arr[0] + arr[1] + arr[2] + arr[3] + arr[4] + arr[5]+ arr[6] + arr[7] + arr[8] + arr[9];
+console.log(result)*/
+
+//2
+/*let book = {
+    name : 'jsBook',
+    string : 200,
+    genre : 'comedy'
+}*/
+//3
+/*let book2 = {
+    name : 'jsBook',
+    string : 200,
+    genre : 'comedy',
+    authors : {
+        user1 : Bob,
+        user2 : job,
+    }
+}*/
+//4
+/*let arrObj = [
+    {
+        name: 'html book',
+        string: 100,
+        genre: 'HyperText Markup Language'
+    },
+    {
+        name: 'css book',
+        string: 200,
+        genre: 'Cascading Style Sheets'
+    },
+    {
+        name: 'js book',
+        string: 300,
+        genre: 'JavaScript'
+    }
+]
+
+let arr1 = `${arrObj[0].name} ${arrObj[0].string} ${arrObj[0].genre}`;
+let arr2 = `${arrObj[1].name} ${arrObj[1].string} ${arrObj[1].genre}`;
+let arr3 = `${arrObj[2].name} ${arrObj[2].string} ${arrObj[2].genre}`;
+console.log(arr1)
+console.log(arr2)
+console.log(arr3)*/
+//5
+/*let width = 10;
+let height = 23;
+let s = width + height
+console.log(s)*/
+//6
+/*let dC = 4;
+let heightC = 10;
+let s = heightC * dC;
+console.log(s)*/
+
+//7
+/*
+// let n = 3;
+// let m = 4;
+// let k = Math.pow(m, n);
+// console.log(k);
+
+// let n = 3;
+// let m = 4;
+// let k = m ** n;
+// console.log(k);
+*/
+
+// 8 errors
+
+// let a = 100;
+// let b = 500;
+// let c = "hello";
+// let d = "okten";
+// let x = 'constant value';
+// {
+//     console.log(a);
+//     x = 'new value';
+// }
+// b = 'new value';
+//
+// let result = 100 + a;
+//
+// console.log(result);
