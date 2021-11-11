04c478c3932dfb10032afcbbf84de6ffffcdf2f8
SerhiiHrynchyshyn <server_059@ukr.net>
2021-11-11 15:44:44 +0200

    js create

---
+++ js-leson-1/arror.js
@@ -0,0 +1,16 @@
+// 8 errors
+
+// let a = 100;
+// let b = 500;
+// let c = "hello";
+// let d = "okten";
+// let x = 'constant value';
+// {
+//     console.log(a);
+//     x = 'new value';
+// }
+// b = 'new value';
+//
+// let result = 100 + a;
+//
+// console.log(result);
---
+++ js-leson-1/classwork.js
@@ -0,0 +1,71 @@
+// homework
+// 1 Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+/*let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let result = arr[0] + arr[1] + arr[2] + arr[3] + arr[4] + arr[5]+ arr[6] + arr[7] + arr[8] + arr[9];
+console.log(result)*/
+
+//2 Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+/*let book = {
+    name : 'jsBook',
+    string : 200,
+    genre : 'comedy'
+}*/
+//3 Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+/*let book2 = {
+    name : 'jsBook',
+    string : 200,
+    genre : 'comedy',
+    authors : {
+        user1 : Bob,
+        user2 : job,
+    }
+}*/
+//4 Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+/*let arrObj = [
+    {
+        name: 'html book',
+        string: 100,
+        genre: 'HyperText Markup Language'
+    },
+    {
+        name: 'css book',
+        string: 200,
+        genre: 'Cascading Style Sheets'
+    },
+    {
+        name: 'js book',
+        string: 300,
+        genre: 'JavaScript'
+    }
+]
+
+let arr1 = `${arrObj[0].name} ${arrObj[0].string} ${arrObj[0].genre}`;
+let arr2 = `${arrObj[1].name} ${arrObj[1].string} ${arrObj[1].genre}`;
+let arr3 = `${arrObj[2].name} ${arrObj[2].string} ${arrObj[2].genre}`;
+console.log(arr1)
+console.log(arr2)
+console.log(arr3)*/
+
+
+//5Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height)
+// та шириною 10см (змінна width). Значення площі зберігати в змінній s.
+/*let width = 10;
+let height = 23;
+let s = width * height;
+console.log(s);*/
+
+//6Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC)
+// та діаметром основи 4м (dC), результат помістіть у змінну v.
+/*let heightC = 10;
+let dC = 4;
+const PI = 3.14;
+let s = PI * dC * heightC;
+
+console.log(s)*/
+
+//7У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
+/*let n = 3;
+let m = 4;
+let k = Math.pow(m, n);
+console.log(k);*/
