b6b938635663e6f145f0e78b65b3ec967aa83b36
SerhiiHrynchyshyn <server_059@ukr.net>
2021-11-27 02:19:12 +0200

    commit classwork

--- js-leson-7/classwork.js
+++ js-leson-7/classwork.js
@@ -1,10 +1,49 @@
-// - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+// - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість,
+// об'єм двигуна. додати в об'єкт функції:
 // -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
 // -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
 // -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
-
+// function Car(model, producer, year, maxSpeed, engineCapacity) {
+//     this.model = model;
+//     this.producer = producer;
+//     this.year = year;
+//     this.maxSpeed = maxSpeed;
+//     this.engineCapacity = engineCapacity;
+//
+//     this.drive = function () {
+//         console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`);
+//     }
+//
+//     this.info = function () {
+//         console.log(`модель машини - ${this.model}, виробник - ${this.producer}, рік випуску - ${this.year}, максимальна швидкість - ${this.maxSpeed}, об'єм двигуна - ${this.engineCapacity}`);
+//     }
+//
+//     this.increaseMaxSpeed = function (newSpeed) {
+//         this.maxSpeed = newSpeed;
+//     }
+//
+//     this.changeYear = function (newValue) {
+//         this.year = newValue;
+//     }
+//
+//     this.addDriver = function (driver) {
+//         this.drive = driver;
+//     }
+// }
+//
+// let car = new Car('Mercedes-AMG GT Coupe', 'Germany', 2020, 300, '350 кВт (476 л. с.)');
+// console.log(car)
+// car.drive();
+// car.info();
+// car.increaseMaxSpeed('500');
+// car.drive();
+// car.changeYear(2021);
+// car.info();
+// car.addDriver();
+// car.addDriver();
+// car.addDriver('Serhii');
 
 // - (Те саме, тільки через клас)
 // Створити клас який дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
@@ -13,7 +52,6 @@
 // -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
-
 
 
 // -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
