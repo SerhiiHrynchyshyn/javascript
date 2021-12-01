1793219ee264dce386a35344bcc6d403ce59fbb5
SerhiiHrynchyshyn <server_059@ukr.net>
2021-12-01 14:00:05 +0200

    add loson 8

--- js-leson-8/additional/additional.js
+++ js-leson-8/additional/additional.js
@@ -1,19 +1,19 @@
 // написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в
 // окремий масив. масив вивести в консоль
 
-let arr = [];
-let reCall = (elem) => {
-    for (let elements of elem.children) {
-        if (elements.className !== '') {
-            let classList = elements.className.split(/\s+/);
-            for (let i = 0; i < classList.length; i++) {
-                arr.push(classList[i]);
-            }
-        }
-        reCall(elements);
-    }
-}
-
-
-reCall(document.body);
-console.log(arr)
+// let arr = [];
+// let reCall = (elem) => {
+//     for (let elements of elem.children) {
+//         if (elements.className !== '') {
+//             let classList = elements.className.split(/\s+/);
+//             for (let i = 0; i < classList.length; i++) {
+//                 arr.push(classList[i]);
+//             }
+//         }
+//         reCall(elements);
+//     }
+// }
+//
+//
+// reCall(document.body);
+// console.log(arr)
