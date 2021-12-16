e27c6871ebdfc7764c79c7b5302afa65b6f20796
SerhiiHrynchyshyn <server_059@ukr.net>
2021-12-16 15:20:59 +0200

    push user file

---
+++ mini-project/userDetails.js
@@ -0,0 +1,39 @@
+// На странице user-details.html:
+// 4 Вывести всю, без исключения, информацию про объект user на кнопку/ссылку которого был совершен клик ранее.
+// 5 Добавить кнопку "post of current user", при клике на которую, появляются title всех постов текущего юзера
+// (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
+// 6 Каждому посту добавить кнопку/ссылку, при клике на которую происходит переход на страницу post-details.html,
+// которая имеет детальную информацию про текущий пост.
+
+let getUser = localStorage.getItem('user');
+let user = JSON.parse(getUser);
+
+let container = document.createElement('div');
+container.classList.add('container');
+
+let postBtn = document.createElement('button');
+postBtn.innerText = 'post of current user';
+
+function getUsers(userObj) {
+    for (let userKey in userObj) {
+        let wrapUser = document.createElement('div');
+        if (typeof userObj[userKey] === 'object' && userObj[userKey] !== null) {
+            getUsers(userObj[userKey]);
+        } else {
+            wrapUser.innerText = userObj[userKey];
+            container.appendChild(wrapUser);
+        }
+    }
+}
+
+getUsers(user)
+
+container.appendChild(postBtn)
+document.body.appendChild(container)
+
+
+
+
+
+
+
