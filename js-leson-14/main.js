c9ffdc59e14b6b3dbaddef384b0248f87971825e
SerhiiHrynchyshyn <server_059@ukr.net>
2021-12-17 00:09:50 +0200

    fix

--- js-leson-14/main.js
+++ js-leson-14/main.js
@@ -6,12 +6,12 @@
 // // Напиклад. // Прикнутись - 0.3с // Поснідати - 1с // Піти в душ - 0.5с // Дочекатись автобус - 3с // Пообідати - 1с // // І так далі
 
 
-function iWokeUp (woke){
+function iWokeUp(woke) {
     return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            if(woke){
+        setTimeout(() => {
+            if (woke) {
                 console.log('Прокинувся');
-                resolve()
+                resolve(true)
             } else {
                 reject('Хватіт спати!');
             }
@@ -21,22 +21,22 @@ function iWokeUp (woke){
 }
 
 
-function teeth (){
+function teeth() {
     return new Promise((resolve) => {
-        setTimeout(() =>{
+        setTimeout(() => {
             console.log('Почистив зуби')
-            resolve();
+            resolve(true);
         }, 200)
     })
 }
 
 
-function cat (feedTheCat){
+function cat(feedTheCat) {
     return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            if(feedTheCat){
+        setTimeout(() => {
+            if (feedTheCat) {
                 console.log('Нагодувати кота')
-                resolve()
+                resolve(true)
             } else {
                 reject('Кіт не з\'їв вчорашній корм')
             }
@@ -45,12 +45,12 @@ function cat (feedTheCat){
 }
 
 
-function exercise (did){
+function exercise(did) {
     return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            if(did){
+        setTimeout(() => {
+            if (did) {
                 console.log('Зарядка');
-                resolve()
+                resolve(true)
             } else {
                 reject('Вали робити зарядку')
             }
@@ -58,14 +58,13 @@ function exercise (did){
     })
 }
 
-function eat (notHungry){
-    return new Promise((resolve, reject) =>{
-        setTimeout(() =>{
-            if(notHungry){
+function eat(notHungry) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (notHungry) {
                 console.log('Поїв');
-                resolve();
-            }
-            else {
+                resolve(true);
+            } else {
                 reject('Лікарня чекає');
             }
         }, 500)
@@ -73,12 +72,12 @@ function eat (notHungry){
 }
 
 
-function coffee (drink){
+function coffee(drink) {
     return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            if(drink){
+        setTimeout(() => {
+            if (drink) {
                 console.log('Кави випив');
-                resolve()
+                resolve(true)
             } else {
                 reject('Не спи')
             }
@@ -86,12 +85,12 @@ function coffee (drink){
     })
 }
 
-function wentToWork (work){
-    return new Promise((resolve,reject) =>{
-        setTimeout(()=>{
-            if(work){
+function wentToWork(work) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (work) {
                 console.log('Пішов на роботу');
-                resolve()
+                resolve(true)
             } else {
                 reject('Прийдеш на роботу а сьогодні не твій день')
             }
@@ -99,11 +98,11 @@ function wentToWork (work){
     })
 }
 
-function wentHome (home){
+function wentHome(home) {
     return new Promise((resolve, reject) => {
-        if(home){
+        if (home) {
             console.log('Поїхав додому')
-            resolve();
+            resolve(true);
         } else {
             reject('Машини нема йди пішки')
         }
@@ -114,11 +113,11 @@ function wentHome (home){
 function work(isTaskDone, callback) {
     return new Promise((resolve) => {
         let money = 0
-        if(isTaskDone){
-            console.log('Шось заробив')
+        if (isTaskDone) {
+            console.log('Заробив ГРОШИИ')
             money += 100;
             callback(money)
-            resolve();
+            resolve(false);
         } else {
             console.log('не заробив нічого?')
             callback(money)
@@ -128,11 +127,11 @@ function work(isTaskDone, callback) {
     })
 }
 
-function goShopping(cash){
+function goShopping(cash) {
     return new Promise((resolve, reject) => {
-        if (cash > 0){
+        if (cash > 0) {
             console.log('приніс кучу бабла ')
-            resolve()
+            resolve(true)
         } else {
             reject('йди заробляй')
         }
@@ -140,16 +139,49 @@ function goShopping(cash){
 }
 
 
-async function morning(){
-    const woke = await iWokeUp(true);
-    const zubki = await teeth();
-    const feed = await cat(true);
-    const charged = await coffee(true);
-    const poiv = await eat(true);
-    const vprava = await exercise(true);
-    const working = await wentToWork(true);
-    const goTohom = await wentHome(true);
-    const job = await work(false, goShopping);
-}
 
-morning();
+//
+//
+iWokeUp(true).then(result => {
+    return teeth(result);
+}).then(result => {
+    return cat(result)
+}).then(result => {
+    return coffee(result);
+}).then(result => {
+    return eat(result);
+}).then(result => {
+    return exercise(result);
+}).then(result => {
+    return wentToWork(result);
+}).then(result => {
+    return wentHome(result);
+}).then(result => {
+    return work(result);
+}).then(result => {
+    return goShopping(result);
+})
+
+//
+//
+
+
+//
+// async function morning() {
+//     try {
+//         const woke = await iWokeUp(true);
+//         const zubki = await teeth();
+//         const feed = await cat(true);
+//         const charged = await coffee(true);
+//         const poiv = await eat(true);
+//         const vprava = await exercise(true);
+//         const working = await wentToWork(true);
+//         const goTohom = await wentHome(true);
+//         const job = await work(false, goShopping);
+//
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
+//
+// morning();
