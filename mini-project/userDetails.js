adeb62b1da84a3bca5bf6aa325cfcd8f9940f931
SerhiiHrynchyshyn <server_059@ukr.net>
2021-12-16 02:02:24 +0200

    received in the user document

--- mini-project/userDetails.js
+++ mini-project/userDetails.js
@@ -5,52 +5,6 @@
 // 6 Каждому посту добавить кнопку/ссылку, при клике на которую происходит переход на страницу post-details.html,
 // которая имеет детальную информацию про текущий пост.
 
-// let getUser = localStorage.getItem('user');
-// let user = JSON.parse(getUser);
-//
-// let container = document.createElement('div');
-// container.classList.add('container');
-//
-// let postBtn = document.createElement('button');
-// postBtn.innerText = 'post of current user';
-//
-// for (let userKey in user) {
-//     let wrapUser = document.createElement('div');
-//     wrapUser.innerText = user[userKey];
-//
-//     container.appendChild(wrapUser);
-// }
-// container.appendChild(postBtn)
-// document.body.appendChild(container)
-
-
-// let getUser = localStorage.getItem('user');
-// let user = JSON.parse(getUser);
-//
-// let container = document.createElement('div');
-// container.classList.add('container');
-//
-// let postBtn = document.createElement('button');
-// postBtn.innerText = 'post of current user';
-// function getUsers () {
-//     for (let userKey in user) {
-//         let wrapUser = document.createElement('div');
-//         if (typeof user[userKey] === 'object' && user[userKey] !== null) {
-//
-//         } else {
-//             wrapUser.innerText = user[userKey];
-//             container.appendChild(wrapUser);
-//         }
-//
-//
-//     }
-// }
-// getUsers(user)
-//
-// container.appendChild(postBtn)
-// document.body.appendChild(container)
-
-
 let getUser = localStorage.getItem('user');
 let user = JSON.parse(getUser);
 
