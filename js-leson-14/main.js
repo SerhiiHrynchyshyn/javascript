9f37bd71504657dfbe823895384cef65bbbf4745
SerhiiHrynchyshyn <server_059@ukr.net>
2021-12-18 14:58:34 +0200

    fix

--- js-leson-14/main.js
+++ js-leson-14/main.js
@@ -6,168 +6,139 @@
 // // Напиклад. // Прикнутись - 0.3с // Поснідати - 1с // Піти в душ - 0.5с // Дочекатись автобус - 3с // Пообідати - 1с // // І так далі
 
 
-function iWokeUp(woke) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (woke) {
-                console.log('Прокинувся');
-                resolve(true)
-            } else {
-                reject('Хватіт спати!');
-            }
-        }, 300)
-
-    })
-}
-
-
-function teeth() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log('Почистив зуби')
-            resolve(true);
-        }, 200)
-    })
-}
-
-
-function cat(feedTheCat) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (feedTheCat) {
-                console.log('Нагодувати кота')
-                resolve(true)
-            } else {
-                reject('Кіт не з\'їв вчорашній корм')
-            }
-        }, 500)
-    })
-}
-
-
-function exercise(did) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (did) {
-                console.log('Зарядка');
-                resolve(true)
-            } else {
-                reject('Вали робити зарядку')
-            }
-        }, 200)
-    })
-}
-
-function eat(notHungry) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (notHungry) {
-                console.log('Поїв');
-                resolve(true);
-            } else {
-                reject('Лікарня чекає');
-            }
-        }, 500)
-    })
-}
-
-
-function coffee(drink) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (drink) {
-                console.log('Кави випив');
-                resolve(true)
-            } else {
-                reject('Не спи')
-            }
-        }, 400)
-    })
-}
-
-function wentToWork(work) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (work) {
-                console.log('Пішов на роботу');
-                resolve(true)
-            } else {
-                reject('Прийдеш на роботу а сьогодні не твій день')
-            }
-        }, 200)
-    })
-}
-
-function wentHome(home) {
-    return new Promise((resolve, reject) => {
-        if (home) {
-            console.log('Поїхав додому')
-            resolve(true);
-        } else {
-            reject('Машини нема йди пішки')
-        }
-    })
-}
-
-
-function work(isTaskDone, callback) {
-    return new Promise((resolve) => {
-        let money = 0
-        if (isTaskDone) {
-            console.log('Заробив ГРОШИИ')
-            money += 100;
-            callback(money)
-            resolve(false);
-        } else {
-            console.log('не заробив нічого?')
-            callback(money)
-            resolve();
-        }
-
-    })
-}
-
-function goShopping(cash) {
-    return new Promise((resolve, reject) => {
-        if (cash > 0) {
-            console.log('приніс кучу бабла ')
-            resolve(true)
-        } else {
-            reject('йди заробляй')
-        }
-    })
-}
-
-// setTimeout(()=> {
-//     console.log('Прокинувся');
-//     setTimeout(() => {
-//         console.log('Почистив зуби');
+// function iWokeUp(woke) {
+//     return new Promise((resolve, reject) => {
 //         setTimeout(() => {
-//             console.log('Нагодувати кота');
-//             setTimeout(() => {
+//             if (woke) {
+//                 console.log('Прокинувся');
+//                 resolve(true)
+//             } else {
+//                 reject('Хватіт спати!');
+//             }
+//         }, 300)
+//
+//     })
+// }
+//
+//
+// function teeth() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             console.log('Почистив зуби')
+//             resolve(true);
+//         }, 200)
+//     })
+// }
+//
+//
+// function cat(feedTheCat) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (feedTheCat) {
+//                 console.log('Нагодувати кота')
+//                 resolve(true)
+//             } else {
+//                 reject('Кіт не з\'їв вчорашній корм')
+//             }
+//         }, 500)
+//     })
+// }
+//
+//
+// function exercise(did) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (did) {
 //                 console.log('Зарядка');
-//                 setTimeout(() => {
-//                     console.log('Поїв');
-//                     setTimeout(() => {
-//                         console.log('Кави випив');
-//                         setTimeout(() => {
-//                             console.log('Пішов на роботу');
-//                             setTimeout(() => {
-//                                 console.log('Поїхав додому');
-//                                 setTimeout(() => {
-//                                     console.log('Заробив ГРОШИИ');
-//                                     setTimeout(() => {
-//                                         console.log('йди заробляй');
-//                                     }, 200);
-//                                 }, 300);
-//                             }, 400);
-//                         }, 200);
-//                     }, 400);
-//                 }, 500);
-//             }, 200);
-//         }, 500);
-//     }, 200);
-// }, 300);
+//                 resolve(true)
+//             } else {
+//                 reject('Вали робити зарядку')
+//             }
+//         }, 200)
+//     })
+// }
+//
+// function eat(notHungry) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (notHungry) {
+//                 console.log('Поїв');
+//                 resolve(true);
+//             } else {
+//                 reject('Лікарня чекає');
+//             }
+//         }, 500)
+//     })
+// }
+//
+//
+// function coffee(drink) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (drink) {
+//                 console.log('Кави випив');
+//                 resolve(true)
+//             } else {
+//                 reject('Не спи')
+//             }
+//         }, 400)
+//     })
+// }
+//
+// function wentToWork(work) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (work) {
+//                 console.log('Пішов на роботу');
+//                 resolve(true)
+//             } else {
+//                 reject('Прийдеш на роботу а сьогодні не твій день')
+//             }
+//         }, 200)
+//     })
+// }
+//
+// function wentHome(home) {
+//     return new Promise((resolve, reject) => {
+//         if (home) {
+//             console.log('Поїхав додому')
+//             resolve(true);
+//         } else {
+//             reject('Машини нема йди пішки')
+//         }
+//     })
+// }
+//
+//
+// function work(isTaskDone, callback) {
+//     return new Promise((resolve) => {
+//         let money = 0
+//         if (isTaskDone) {
+//             console.log('Заробив ГРОШИИ')
+//             money += 100;
+//             callback(money)
+//             resolve(false);
+//         } else {
+//             console.log('не заробив нічого?')
+//             callback(money)
+//             resolve();
+//         }
+//
+//     })
+// }
+//
+// function goShopping(cash) {
+//     return new Promise((resolve, reject) => {
+//         if (cash > 0) {
+//             console.log('приніс кучу бабла ')
+//             resolve(true)
+//         } else {
+//             reject('йди заробляй')
+//         }
+//     })
+// }
+
+
 // -----------------------------
 // iWokeUp(true).then(result => {
 //     return teeth(result);
@@ -207,3 +178,160 @@ function goShopping(cash) {
 // }
 //
 // morning();
+
+// --------------------------------
+function iWokeUp(e, cb) {
+    setTimeout(() => {
+        if (e) {
+            cb('Прокинувся');
+        } else {
+            cb('Хватіт спати!');
+        }
+    }, 300)
+}
+
+
+function teeth(e, cb) {
+    setTimeout(() => {
+        if (e) {
+            cb('Почистив зуби');
+        } else {
+            cb('не ваняй');
+        }
+    }, 200)
+}
+
+
+function cat(e, cb) {
+    setTimeout(() => {
+        if (e) {
+            cb('Нагодувати кота');
+        } else {
+            cb('Кіт не з\'їв вчорашній корм');
+        }
+    }, 500)
+}
+
+
+function exercise(e, cb) {
+    setTimeout(() => {
+        if (e) {
+            cb('Зарядка');
+        } else {
+            cb('Вали робити зарядку');
+        }
+    }, 200)
+}
+
+function eat(e, cb) {
+    setTimeout(() => {
+        if (e) {
+            cb('Поїв');
+        } else {
+            cb('Лікарня чекає');
+        }
+    }, 500)
+}
+
+
+function coffee(e, cb) {
+    setTimeout(() => {
+        if (e) {
+            cb('Кави випив');
+        } else {
+            cb('Не спи');
+        }
+    }, 400)
+}
+
+function wentToWork(e, cb) {
+    setTimeout(() => {
+        if (e) {
+            cb('Пішов на роботу');
+        } else {
+            cb('Прийдеш на роботу а сьогодні не твій день');
+        }
+    }, 200)
+}
+
+function wentHome(e, cb) {
+    if (e) {
+        cb('Поїхав додому');
+    } else {
+        cb('Машини нема йди пішки');
+    }
+}
+
+
+function work(e, cb) {
+    if (e) {
+        cb('Заробив ГРОШИИ');
+    } else {
+        cb('не заробив нічого?');
+    }
+}
+
+function goShopping(e, cb) {
+    if (e) {
+        cb('приніс кучу бабла ');
+    } else {
+        cb('йди заробляй');
+    }
+}
+
+
+iWokeUp(true, (error, callback) => {
+    if (!error) {
+        console.log(callback)
+
+        teeth(true, (error, callback) => {
+            if (!error) {
+                console.log(callback);
+
+                cat(true, (error, callback) => {
+                    if (!error) {
+                        console.log(callback);
+
+                        exercise(true, (error, callback) => {
+                            if (!error) {
+                                console.log(callback);
+
+                                eat(true, (error, callback) => {
+                                    if (!error) {
+                                        console.log(callback);
+
+                                        coffee(true, (error, callback) => {
+                                            if (!error) {
+                                                console.log(callback);
+
+                                                wentToWork(true, (error, callback) => {
+                                                    if (!error) {
+                                                        console.log(callback);
+
+                                                        wentHome(true, (error, callback) => {
+                                                            if (!error) {
+                                                                console.log(callback);
+
+                                                                work(true, (error, callback) => {
+                                                                    if (!error) {
+                                                                        console.log(callback);
+
+                                                                        goShopping
+                                                                    }
+                                                                });
+                                                            }
+                                                        });
+                                                    }
+                                                });
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+});
